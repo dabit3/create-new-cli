2299ab23a526f73ed9f0cc7a7e7ef230a3067b06
nader dabit <dabit3@gmail.com>
2017-06-10 14:54:59 -0500

    fixed error creating new app

--- src/bin/build.js
+++ src/bin/build.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 require('shelljs/global');
-const paths = require('path');
+const path = require('path');
 const fs = require('fs');
 const figlet = require('figlet');
 const chalk = require('chalk');
